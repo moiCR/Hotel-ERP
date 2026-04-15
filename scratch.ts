6804e173bce217e7d3abd0ba4c333ee6bfbe37eb
moiCR <ssnowk05@gmail.com>
2026-04-14 20:15:50 -0600

    Unnecessary code has been removed

--- scratch.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-import { db } from './server/utils/db'
-
-async function main() {
-    console.log("DATABASE_URL:", process.env.DATABASE_URL)
-    const logs = await db.bitacora.findMany()
-    console.log("DB logs count:", logs.length)
-}
-main().catch(console.error).finally(() => process.exit(0))
